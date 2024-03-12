c459f586d0c119235fe56c22023c9a3f352844c4
tearfu1 <axuior12@gmail.com>
2024-03-12 20:00:45 +0200

    added: log Error func database configuration

--- cmd/main.go
+++ cmd/main.go
@@ -4,6 +4,8 @@ import (
 	"log/slog"
 	"os"
 	"url-shortener/internal/config"
+	"url-shortener/internal/lib/logger/sl"
+	"url-shortener/internal/storage/sqlite"
 )
 
 const (
@@ -18,6 +20,13 @@ func main() {
 	log := setupLogger(cfg.Env)
 	log.Info("Starting url-shortener", slog.String("env", cfg.Env))
 	log.Debug("debug messages are enabled")
+
+	storage, err := sqlite.New(cfg.StoragePath)
+	if err != nil {
+		log.Error("failed to init storage", sl.Err(err))
+		os.Exit(1)
+	}
+	_ = storage
 }
 
 func setupLogger(env string) *slog.Logger {
