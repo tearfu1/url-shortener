3ba668310433a7f6d24e10b33f34dc91945f6194
tearfu1 <axuior12@gmail.com>
2024-03-12 19:33:33 +0200

    true added

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,41 @@
-package cmd
+package main
+
+import (
+	"log/slog"
+	"os"
+	"url-shortener/internal/config"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
+
+func main() {
+	cfg := config.MustLoad()
+
+	log := setupLogger(cfg.Env)
+	log.Info("Starting url-shortener", slog.String("env", cfg.Env))
+	log.Debug("debug messages are enabled")
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+
+	return log
+}
