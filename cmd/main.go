ef6cbdbbb1c4a48b26aad6fc3cfeb3cbc0474b02
tearfu1 <axuior12@gmail.com>
2024-03-15 16:57:14 +0200

    change log:
    added authorisation Basic Auth
    
    handlers POST and DELETE can be done by pass authorisation
    
    redirect handler test added

--- cmd/main.go
+++ cmd/main.go
@@ -45,9 +45,15 @@ func main() {
 	router.Use(middleware.Recoverer)
 	router.Use(middleware.URLFormat)
 
-	router.Post("/url", save.New(log, storage))
+	router.Route("/url", func(r chi.Router) {
+		r.Use(middleware.BasicAuth("url-shortener", map[string]string{
+			cfg.HTTPServer.User: cfg.HTTPServer.Password,
+		}))
+		r.Post("/", save.New(log, storage))
+		r.Delete("/{alias}", dl.New(log, storage))
+	})
+
 	router.Get("/{alias}", redirect.New(log, storage))
-	router.Delete("/{alias}", dl.New(log, storage))
 
 	log.Info("starting server", slog.String("address", cfg.Address))
 
